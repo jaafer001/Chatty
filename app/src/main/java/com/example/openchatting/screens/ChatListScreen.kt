4a6946e4f63e9b1d1cc269ac7cf16c6c83ddbd00
jaafer001 <jaafergood@gmail.com>
2026-03-04 19:35:11 +0000

    ff

--- app/src/main/java/com/example/openchatting/screens/ChatListScreen.kt
+++ app/src/main/java/com/example/openchatting/screens/ChatListScreen.kt
@@ -16,26 +16,147 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun ChatListScreenContent(modifier: Modifier = Modifier) {
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            textAlign = TextAlign.Center,
-            text = "ChatListScreen",
-            fontSize = 30.sp,
-            fontFamily = FontFamily.Cursive,
-            modifier = Modifier.padding(16.dp)
-        )
+fun ChatList(
+    accountList: List<Account>
+) {
+    val isDarkMode = isSystemInDarkTheme()
+    val appBarColor = if (isDarkMode) Color.DarkGray else Color.LightGray
+    val appBarTextColor = if (isDarkMode) Color.White else Color.Black
+    val backgroundColor = if (isDarkMode) Color(0xFF121212) else Color(0xFFFAFAFA)
+
+    var isAppBarHidden by remember { mutableStateOf(false) }
+    val nestedScrollConnection = remember {
+        object : NestedScrollConnection {
+
+            override fun onPreScroll(available: Offset, source: NestedScrollSource): Offset {
+                val delta = available.y
+
+                if (delta < -5) {
+                    isAppBarHidden = true
+                }
+
+                else if (delta > 5) {
+                    isAppBarHidden = false
+                }
+
+                return Offset.Zero
+            }
+        }
+    }
+    Scaffold(
+        modifier = Modifier
+            .nestedScroll(nestedScrollConnection),
+        containerColor = backgroundColor,
+        topBar = {
+
+            AnimatedVisibility(
+                visible = !isAppBarHidden,
+                enter = fadeIn() + slideInVertically(),
+                exit = fadeOut() + slideOutVertically()
+            ) {
+                TopAppBar(
+                    title = { Text(
+                        "Chatty",
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                        fontFamily = FontFamily.Cursive,
+                        color = appBarTextColor
+                    ) },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = appBarColor,
+                        titleContentColor = appBarTextColor
+                    ),
+                )
+            }
+        }
+    ) { paddingValues ->
+
+        LazyColumn(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            items(accountList) { account ->
+                ChatMessageCard(
+                    accountDetails = Account(
+                        account.name,
+                        account.profilePicture,
+                        account.lastMessage,
+                        account.lastMessageTime
+                    )
+                )
+            }
+        }
     }
 }
 
-
 @Composable
-fun ChatMessageCard(modifier: Modifier = Modifier) {
+fun ChatMessageCard(
+    accountDetails: Account
+) {
+    val isDarkMode = isSystemInDarkTheme()
+    val primaryTextColor = if (isDarkMode) Color.White else Color.Black
+    val secondaryTextColor = if (isDarkMode) Color.LightGray else Color.Gray
 
+    val painter = painterResource(id = accountDetails.profilePicture)
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp),
+        shape = RoundedCornerShape(25.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Box(
+                modifier = Modifier
+            ){
+                Image(
+                    painter = painter,
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier
+                        .size(80.dp)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+            }
+            Column(
+                modifier = Modifier.padding(start = 10.dp)
+                    .fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Text(
+                    modifier = Modifier,
+                    text = accountDetails.name,
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    fontFamily = FontFamily.Cursive,
+                    textAlign = TextAlign.Start,
+                    color = primaryTextColor
+                )
+                Text(
+                    modifier = Modifier,
+                    text = accountDetails.lastMessage,
+                    fontSize = 18.sp,
+                    fontFamily = FontFamily.Cursive,
+                    color = secondaryTextColor
+                )
+            }
+            Text(
+                modifier = Modifier.weight(1f),
+                text = accountDetails.lastMessageTime,
+                textAlign = TextAlign.End,
+                fontSize = 18.sp,
+                fontFamily = FontFamily.Cursive,
+                color = secondaryTextColor
+            )
+        }
+    }
 }
 
 @Suppress("UNUSED_PARAMETER")
